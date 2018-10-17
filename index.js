07754f8df1b0f392ac9acebe8ef712c1b2dba25b
lamsam2016 <linyusen@youmi.net>
2018-10-17 17:30:41 +0800

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+const Tdex = require('./util/apis')
+
+
+
+var tdex = new Tdex({
+	apiKey: '7AGFdayY1ePgJbCDsa1gVivym9UhUSG3oazY3fwAy1dpX5UxsmHa7EzuQVy1XzoJ',
+	apiSecret: '7AGFdbCfPWCB2wh4W2Ln5roMRhnhQVvMvWCsNFpEnc4MLyE28TZX38v92uZ48NXf',
+})
+
+//var config = {
+//	cid: 1,
+//	side: 0,
+//	scale: 10,
+//	volume: 1,
+//	visible: -1,
+//}
+
+
+
+var config = {"currency":1,"direction":2,"amount":1}
+
+
+tdex.walletSwitch(config ,res => {
+	console.log(JSON.stringify(res))
+})
+
+
+
+//module.exports = Tdex
